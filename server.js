a320e9544e12a520a36b132b0b6dacc9d3d7b7ac
Dário Prazeres <darioedgarprazeres@gmail.com>
2022-05-19 15:59:53 +0100

    update: install cors midleware to help to share data

--- server.js
+++ server.js
@@ -2,17 +2,23 @@ require('dotenv').config();
 const express = require('express');
 var app = express();
 var mongoose = require('mongoose');
+var cors = require('cors');
 var port = process.env.PORT || 3000;
-mongoose.connect('mongodb+srv://DarioPrazeres:Fernandes1973@cluster0.xvrcp.mongodb.net/market?retryWrites=true&w=majority', {useNewUrlParser: true});
-const db = mongoose.connection
-db.on('error', (error) => console.error(error));
+var dev_db_url = 'mongodb://127.0.0.1/subscribers';
+var mongoDB = dev_db_url;
+
+mongoose.connect(mongoDB, {useNewUrlParser:true});                                                                     
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB conection error:')); 
 db.once('open', () => console.log('Conneted to database'));
 
 app.use(express.json())
-
+app.use(express.urlencoded({ extended: false }));
+app.use(cors())
 var subsRouter = require('./router/subscribers');
 app.use('/subscribers', subsRouter)
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
+    console.log('api')
+    res.send({ message: "Hello from server!" });
 });
-app.listen(port, () => {console.log('Server Started!')})
+app.listen(3000, () => {console.log('Server Started!')})
